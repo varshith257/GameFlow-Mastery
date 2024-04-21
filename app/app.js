efa1ce9f6ec7bdeda75d5c606bce872b6219d0c7
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-04-21 22:32:14 +0530

    Switched to Socket.IO for improved chat features.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
-const socket = new WebSocket('ws://localhost:3000')
+const socket = io('ws://localhost:3500')
 
 function sendMessage(e) {
     e.preventDefault()
     const input = document.querySelector('input')
     if (input.value) {
-        socket.send(input.value)
+        socket.emit('message', input.value)
         input.value = ''
     }
     input.focus()
@@ -14,7 +14,7 @@ document.querySelector('form')
     .addEventListener('submit', sendMessage)
 
 // Listen for messages
-socket.addEventListener("message", ({data}) => {
+socket.on("message", (data) => {
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
-const ws = require('ws')
-const server = new ws.Server({ port: '3000' })
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+const httpServer = createServer()
 
-server.on('connection', (socket) => {
-    socket.on('message', (message) => {
-        const b = Buffer.from(message)
-        console.log(b.toString())
-        // console.log('received: ', message)
-        socket.send(`${ message }`)
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === 'production' ? flase : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`)
+    socket.on('message', data => {
+        console.log(`${data} from ${socket.id}`)
+        socket.emit('message', `${socket.id.substring(0, 5)} :${data}`)
     })
+})
 
-    
+httpServer.listen(3500, () => {
+    console.log('Server is running on port 3500')
 })
