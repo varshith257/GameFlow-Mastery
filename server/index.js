231a6b80438c60986292f7f103bb2c2e79287ca3
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-04-21 21:51:12 +0530

    Initial commit: Implemented basic chat functionality using WebSockets.

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const ws = require('ws')
+const server = new ws.Server({ port: '3000' })
+
+server.on('connection', (socket) => {
+    socket.on('message', (message) => {
+        const b = Buffer.from(message)
+        console.log(b.toString())
+        // console.log('received: ', message)
+        socket.send(`${ message }`)
+    })
+
+    
+})
